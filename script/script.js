6c8889a2856136a5a10a2982566f6735d9169618
CoderStreetBoys <cyrusjohn016@gmail.com>
2026-09-13 23:44:59 +0800

    feat: add system clock, Wifi-Icon and Taskbar

--- script/script.js
+++ script/script.js
@@ -1,8 +1,27 @@
-let time = document.querySelector('#time');
- function updatetime () {
-    let timetodisplay = new Date().toLocaleString();
-    time.innerText = timetodisplay;
+let time = document.querySelector("#time");
 
+function updatetime() {
+  let now = new Date();
+
+  // Format The Date: "Sep 13, 2026"
+  let dateString = now.toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  // Format ng Time (12-hour with AM/PM, walang seconds): "10:45 PM"
+  let timeString = now.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
+
+  // Date and Time connection
+  time.innerText = `${dateString}  ${timeString}`;
 }
+
 updatetime();
-setInterval(updatetime , 1000);
+setInterval(updatetime, 1000);
+
+const wifiIcon = document.querySelector("#wifi-status");

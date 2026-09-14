116941bf25fbab4c75e59750f386cb848c739c77
MADHAV ANAND <m47903237@gmail.com>
2026-09-14 13:54:27 +0530

    Remove terminal window from HTML and comment out drag functionality in script

--- script/script.js
+++ script/script.js
@@ -10,41 +10,41 @@ function showWelcomeScreen() {
   welcomeScreen.classList.add('hide');
   }
 }
-dragElement(document.getElementById('welcome'));
-function dragElement(element){
-  let initialX = 0 , initialY = 0;
-  let currentX = 0 , currentY = 0;
-  let drag = document.getElementById(element.id + 'header');
-  if(drag){
-    drag.onmousedown = startDragging;
-  } else{
-    element.onmousedown = startDragging;
-  }
+// dragElement(document.getElementById('welcome-screen'));
+// function dragElement(element){
+//   let initialX = 0 , initialY = 0;
+//   let currentX = 0 , currentY = 0;
+//   let drag = document.getElementById(element.id + 'header');
+//   if(drag){
+//     drag.onmousedown = startDragging;
+//   } else{
+//     element.onmousedown = startDragging;
+//   }
 
-  function startDragging(e){
-    e.preventDefault();
-    initialX = e.clientX;
-    initialY = e.clientY;
-    document.onmouseup = stopDragging;
-    document.onmousemove = moveElement;
-  }
+//   function startDragging(e){
+//     e.preventDefault();
+//     initialX = e.clientX;
+//     initialY = e.clientY;
+//     document.onmouseup = stopDragging;
+//     document.onmousemove = moveElement;
+//   }
    
 
-  function moveElement(e){
-    currentX = initialX - e.clientX;
-    currentY = initialY - e.clientY;
+//   function moveElement(e){
+//     currentX = initialX - e.clientX;
+//     currentY = initialY - e.clientY;
 
-    initialX = e.clientX;
-    initialY =  e.clientY;
+//     initialX = e.clientX;
+//     initialY =  e.clientY;
 
-    element.style.top = (element.offsetTop - currentY) + 'px';
-    element.style.left = (element.offsetLeft - currentX) + 'px';
-  }
- function stopDragging(){
-  document.onmousemove = null;
-  document.onmouseup = null;
- }
-}
+//     element.style.top = (element.offsetTop - currentY) + 'px';
+//     element.style.left = (element.offsetLeft - currentX) + 'px';
+//   }
+//  function stopDragging(){
+//   document.onmousemove = null;
+//   document.onmouseup = null;
+//  }
+// }
 updatetime();
 setTimeout(showWelcomeScreen, 3000);
 setInterval(updatetime , 1000);

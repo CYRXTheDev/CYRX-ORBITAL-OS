33f5082a73712d233f98d44e408a509875a0cd40
MADHAV ANAND <m47903237@gmail.com>
2026-09-14 11:29:10 +0530

    Add welcome screen

--- script/script.js
+++ script/script.js
@@ -1,8 +1,15 @@
 let time = document.querySelector('#time');
+let welcomeScreen = document.querySelector('.welcome-screen');
  function updatetime () {
     let timetodisplay = new Date().toLocaleString();
     time.innerText = timetodisplay;
 
 }
+function showWelcomeScreen() {
+  if(welcomeScreen){
+  welcomeScreen.classList.add('hide');
+  }
+}
 updatetime();
+setTimeout(showWelcomeScreen, 3000);
 setInterval(updatetime , 1000);

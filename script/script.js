13c5b08f827ab99be854bb80911d95e812d2e29f
MADHAV ANAND <m47903237@gmail.com>
2026-09-14 12:20:26 +0530

    Add drag window

--- script/script.js
+++ script/script.js
@@ -10,6 +10,25 @@ function showWelcomeScreen() {
   welcomeScreen.classList.add('hide');
   }
 }
+drageElement(document.getElementById('welcome'));
+function dragElement(element){
+  let initialX = 0 , initialY = 0;
+  let currentX = 0 , currentY = 0;
+  let drag = document.getElementById(element.id + 'header');
+  if(drag){
+    drag.onmousedown = startDragging;
+  } else{
+    element.onmousedown = startDragging;
+  }
+
+  function startDargging(e){
+    e.preventDefault;
+    initialX = e.clientX;
+    initialY = e.clientY;
+    
+  }
+
+}
 updatetime();
 setTimeout(showWelcomeScreen, 3000);
 setInterval(updatetime , 1000);

2aa5d8ccea79f99d40ed426be141516b50b70d1c
MADHAV ANAND <m47903237@gmail.com>
2026-09-17 16:05:46 +0530

    feat: implement drag functionality in module and close functionality for welcome screen

---
+++ script/modules/closeElement.js
@@ -0,0 +1,3 @@
+export const closeElement =  (element) => {
+  element.classList.add('hide');
+};
--- script/script.js
+++ script/script.js
@@ -1,50 +1,13 @@
 let time = document.querySelector('#time');
-let welcomeScreen = document.querySelector('.welcome-screen');
+let closewelcomewindow = document.querySelector('#close-welcome');
+let welcomescreen = document.querySelector('#welcome-screen');
  function updatetime () {
     let timetodisplay = new Date().toLocaleString();
     time.innerText = timetodisplay;
 
 }
-function showWelcomeScreen() {
-  if(welcomeScreen){
-  welcomeScreen.classList.add('hide');
-  }
-}
-// dragElement(document.getElementById('welcome-screen'));
-// function dragElement(element){
-//   let initialX = 0 , initialY = 0;
-//   let currentX = 0 , currentY = 0;
-//   let drag = document.getElementById(element.id + 'header');
-//   if(drag){
-//     drag.onmousedown = startDragging;
-//   } else{
-//     element.onmousedown = startDragging;
-//   }
-
-//   function startDragging(e){
-//     e.preventDefault();
-//     initialX = e.clientX;
-//     initialY = e.clientY;
-//     document.onmouseup = stopDragging;
-//     document.onmousemove = moveElement;
-//   }
-   
-
-//   function moveElement(e){
-//     currentX = initialX - e.clientX;
-//     currentY = initialY - e.clientY;
-
-//     initialX = e.clientX;
-//     initialY =  e.clientY;
-
-//     element.style.top = (element.offsetTop - currentY) + 'px';
-//     element.style.left = (element.offsetLeft - currentX) + 'px';
-//   }
-//  function stopDragging(){
-//   document.onmousemove = null;
-//   document.onmouseup = null;
-//  }
-// }
+import { dragElement } from "./modules/dragElement.js";
+import { closeElement } from "./modules/closeElement.js";
 updatetime();
-setTimeout(showWelcomeScreen, 3000);
+closewelcomewindow.addEventListener('click' , () => closeElement(welcomescreen) );
 setInterval(updatetime , 1000);

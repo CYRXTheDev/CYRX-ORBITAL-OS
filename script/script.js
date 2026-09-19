10a9ec25442e11861270bcd16a0a88fc880d250b
MADHAV ANAND <m47903237@gmail.com>
2026-09-19 11:42:45 +0530

    refactor: clean up HTML structure and enhance JavaScript functionality for sidebar and taskbar

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
 let time = document.querySelector('#time');
 let closewelcomewindow = document.querySelector('#close-welcome');
 let welcomescreen = document.querySelector('#welcome-screen');
+let sidebar = document.querySelectorAll('.sidebar ul li a');
+let taskbar = document.querySelectorAll('.taskbar .taskbar-apps .taskbar-icons');
+
  function updatetime () {
     let timetodisplay = new Date().toLocaleString();
     time.innerText = timetodisplay;
@@ -8,6 +11,20 @@ let welcomescreen = document.querySelector('#welcome-screen');
 }
 import { dragElement } from "./modules/dragElement.js";
 import { closeElement } from "./modules/closeElement.js";
+
+taskbar.forEach((element) => {
+    element.addEventListener('click' , () => {
+        element.classList.remove('hide');
+    })
+})
+
+sidebar.forEach((element) => {
+element.addEventListener('click' , () => {
+    element.classList.remove('hide');
+})
+})
+
+
 updatetime();
 closewelcomewindow.addEventListener('click' , () => closeElement(welcomescreen) );
 setInterval(updatetime , 1000);
